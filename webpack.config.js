e4e3db65c9196c9e14f5da379a1b4e169fe1a460
afabrizio <andrew.m.fabrizio@gmail.com>
2016-09-29 11:08:51 -0700

    set up the dev environment with the proper dependencies

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: "./dist/public/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    port: 3000,
+    contentBase: "./dist/public",
+    inline: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ["es2015", "react"]
+        }
+      }
+    ]
+  }
+}
